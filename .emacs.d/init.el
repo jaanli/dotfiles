22c1ba67f0ee82ae9aae8e820d408c2c6f99ef74
Jaan <altosaar@users.noreply.github.com>
2018-05-29 16:47:55 -0400

    add emacs init.el file

---
+++ .emacs.d/init.el
@@ -0,0 +1,41 @@
+(require 'package)
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (proto (if no-ssl "http" "https")))
+  ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
+  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
+  ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
+  (when (< emacs-major-version 24)
+    ;; For important compatibility libraries like cl-lib
+    (add-to-list 'package-archives '("gnu" . (concat proto "://elpa.gnu.org/packages/")))))
+(package-initialize)
+
+; list the packages you want
+(setq package-list
+      '(ein helm color-theme-solarized))
+
+; activate all the packages
+(package-initialize)
+
+; fetch the list of packages available 
+(unless package-archive-contents
+  (package-refresh-contents))
+
+; install the missing packages
+(dolist (package package-list)
+  (unless (package-installed-p package)
+    (package-install package)))
+
+(setq ein:jupyter-default-server-command "jupyter --no-browser")
+
+(require 'helm-config)
+
+(ido-mode 1)
+(setq ido-everywhere t)
+(setq ido-enable-flex-matching t)
+
+(custom-set-variables
+ '(python-guess-indent nil)
+ '(python-indent 2))
+
+(load-theme 'solarized t)
